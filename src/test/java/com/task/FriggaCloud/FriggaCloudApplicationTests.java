042503ea99df40e3a595180c85858e0d505d15b1
Sughosh <sughoshathreya1@gmail.com>
2025-07-07 19:15:34 +0530

    created jar file and docker file

--- src/test/java/com/task/FriggaCloud/FriggaCloudApplicationTests.java
+++ src/test/java/com/task/FriggaCloud/FriggaCloudApplicationTests.java
@@ -1,9 +1,10 @@
 package com.task.FriggaCloud;
 
+import com.task.knowledgebase.FriggaCloudApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FriggaCloudApplication.class)
 class FriggaCloudApplicationTests {
 
 	@Test
